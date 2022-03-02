c185b7aebcc09084388e82c3f695e58255514963
NotJamesB <97855508+NotJamesB@users.noreply.github.com>
2022-03-02 01:31:52 -0800

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,25 @@
+for(var i=0; i<21; i++) {
+    console.log(i)
+}
+
+for(var i=100; i>0; i--) {
+    if (i % 3 ==0) {
+        console.log(i)
+    }
+}
+
+for(var i=4; i>-3.6; i-=1.5) {
+    console.log(i);
+}
+
+var sum=0
+for(var i=0; i<102; i++) {
+    console.log(sum)
+    sum +=i
+}
+
+var multiply=1
+for(var i=1; i<14; i++) {
+    console.log(multiply)
+    multiply *=i
+}
